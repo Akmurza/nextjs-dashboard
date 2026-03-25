d4e2683769338951d3be655405ee31e34d54ca13
Akmurza <akuklin@byupathway.edu>
2026-03-26 02:34:34 +0400

    DB Added and 8-10 chapters done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
-}
+// This file is not used - see (overview)/page.tsx for the dashboard content
+// The (overview) route group handles /dashboard path
+export { default } from './(overview)/page';
